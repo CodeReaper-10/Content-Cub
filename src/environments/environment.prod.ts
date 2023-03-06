d0ad8f7ad16b6c05ef38ccc8cfb22bb58bfe54a4
CodeReaper-10 <adipta1010@gmail.com>
2023-03-06 11:10:10 +0530

    Reverted back redirect_uri for localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
     domain: 'dev-7chx73disu0tz723.us.auth0.com',
     clientId: '5jYYbNoxa2iarq0YcWnWoZDxt6G0XFxc',
     authorizationParams: {
-      //redirect_uri: window.location.origin
-      redirect_uri: "https://akshitagupta15june.github.io/Content-Cub/"
+      redirect_uri: window.location.origin
+      //redirect_uri: "https://akshitagupta15june.github.io/Content-Cub/"
     }
   }
 };
